620ff785578c2be9b1224abeae3c1093c01ee872
Vadim <marakushin.vadim@gmail.com>
2026-03-30 11:12:49 +0300

    add user model

---
+++ app/models/user.py
@@ -0,0 +1,31 @@
+from app.database import Base
+
+from enum import Enum
+
+from typing import Literal, get_args
+
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Role(str, Enum):
+    admin = 'admin'
+    manager = 'manager'
+    user = 'user'
+
+
+RoleType = Literal['admin', 'manager', 'user']
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    role: Mapped[RoleType] = mapped_column(
+        Enum(*get_args(RoleType), name='role_enum', metadata=Base.metadata),
+        default='user',
+    )
