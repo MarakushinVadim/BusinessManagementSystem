eeb3acaf0655142da39d01e1f4b19942060d785b
Vadim <marakushin.vadim@gmail.com>
2026-04-17 14:26:03 +0300

    Remove TeamRoleType from TeamModel and add to UserModel

--- app/models/team.py
+++ app/models/team.py
@@ -1,34 +1,18 @@
-import enum
-
 from uuid import UUID
 
 from fastapi_users_db_sqlalchemy.generics import GUID
 
 from app.config import Base
 
-from sqlalchemy import Enum, ForeignKey
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-from typing import Literal, get_args
-
-
-class TeamRole(str, enum.Enum):
-    manager = "manager"
-    employee = "employee"
-
-
-RoleType = Literal["manager", "employee"]
 
 
 class TeamModel(Base):
     __tablename__ = "teams"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    team_role: Mapped[RoleType] = mapped_column(
-        Enum(*get_args(TeamRole), name="team_role_enum", native_enum=False),
-        default=TeamRole.employee,
-        nullable=False,
-    )
+
     admin_id: Mapped[UUID] = mapped_column(
         GUID(), ForeignKey("users.id"), nullable=True
     )
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,7 @@
 from app.config import Base
 
+import enum
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
 
 from sqlalchemy import Enum, ForeignKey
@@ -10,13 +12,20 @@ from typing import Literal, get_args
 from sqlalchemy.orm import Mapped, mapped_column
 
 
-class Role(str, Enum):
+class Role(str, enum.Enum):
     admin = "admin"
     manager = "manager"
     user = "user"
 
 
+class TeamRole(str, enum.Enum):
+    manager = "manager"
+    employee = "employee"
+
+
 RoleType = Literal["admin", "manager", "user"]
+
+TeamRoleType = Literal["manager", "employee"]
 
 
 class UserModel(SQLAlchemyBaseUserTableUUID, Base):
@@ -26,6 +35,12 @@ class UserModel(SQLAlchemyBaseUserTableUUID, Base):
         Enum(*get_args(RoleType), name="role_enum", metadata=Base.metadata),
         default="user",
     )
+    team_role: Mapped[TeamRoleType] = mapped_column(
+        Enum(*get_args(TeamRoleType), name="team_role_enum", native_enum=False),
+        default=TeamRole.employee,
+        nullable=True,
+    )
+
     team_id: Mapped[int] = mapped_column(ForeignKey("teams.id"), nullable=True)
 
     team: Mapped["TeamModel"] = relationship("TeamModel", back_populates="users")
